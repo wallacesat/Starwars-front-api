1e185beada07fc79ae9db0d202ece83fa625c158
Wallace <wallacesat@live.co.uk>
2019-04-01 09:52:34 -0300

    Ajuste no arquivo de rotas

---
+++ swapiweb/src/services/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import DetailsView from "../views/details/DetailsView";
+import HomeView from "../views/home/HomeView";
+import TablesView from "../views/itemsTable/TablesView";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route exact path="/:object" component={TablesView} />
+      <Route exact path="/:object/:id" component={DetailsView} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
