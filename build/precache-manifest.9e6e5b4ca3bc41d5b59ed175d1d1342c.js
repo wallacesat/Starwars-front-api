b88a885a1b3aa3df1a0009b7719ce8dd4922fd01
Wallace <wallacesat@live.co.uk>
2019-04-05 12:57:29 -0300

    Pasta /build removida do .gitignore

---
+++ build/precache-manifest.9e6e5b4ca3bc41d5b59ed175d1d1342c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f707011d3331a6509a66",
+    "url": "/static/css/main.ba5e1e4b.chunk.css"
+  },
+  {
+    "revision": "f707011d3331a6509a66",
+    "url": "/static/js/main.3722ef21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "013934caee847502a4b4",
+    "url": "/static/css/2.a10977bb.chunk.css"
+  },
+  {
+    "revision": "013934caee847502a4b4",
+    "url": "/static/js/2.28592f29.chunk.js"
+  },
+  {
+    "revision": "656488158cab77602c16136da8d6fa4f",
+    "url": "/static/media/swlogo.65648815.png"
+  },
+  {
+    "revision": "f9ce68fc50fe38523d4f417b6d779604",
+    "url": "/static/media/_planet.f9ce68fc.png"
+  },
+  {
+    "revision": "53a5b1135b4c24b3e3ab4441656c699b",
+    "url": "/static/media/_starship.53a5b113.png"
+  },
+  {
+    "revision": "48706be42eb81740d968824d69febd93",
+    "url": "/static/media/_people.48706be4.png"
+  },
+  {
+    "revision": "26e0db62bb25b2bb0a34c12aa2855263",
+    "url": "/index.html"
+  }
+];
